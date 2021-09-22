69e068c8d15114bd7d23872cd853bceced1fa5f8
panla <pankla@163.com>
2021-09-22 18:35:53 +0800

    1 update find.py 2 add 同步异步阻塞非阻塞

--- code_utils/find.py
+++ code_utils/find.py
@@ -1,16 +1,15 @@
 """
 遍历单个/多个文件夹下的所有文件（可以使用utf-8 read 的文件）
 根据参数中的 targets 在读取的文件中查找 target
-如果查找到就把 target 和 文件路径记录到 log 中
 """
 
 import os
+import sys
 import argparse
 
 param_parser = argparse.ArgumentParser()
 param_parser.add_argument('-d', '--dirs', nargs='*', type=str, required=False, default=[], help='dirs')
 param_parser.add_argument('-t', '--targets', nargs='+', required=True, help='target')
-param_parser.add_argument('-l', '--log', type=str, required=False, default='./r.log', help='log file')
 
 params = param_parser.parse_args()
 dirs = params.dirs
@@ -20,7 +19,6 @@ if dirs:
         if os.path.isdir(os.path.abspath(d)):
             directions.append(os.path.abspath(d))
 targets = params.targets
-log_file = params.log
 
 
 def read_file(path: str):
@@ -31,13 +29,6 @@ def read_file(path: str):
         return ''
 
 
-def write_file(path: str, content: str):
-    with open(path, 'a+', encoding='utf-8') as f:
-        f.write(content)
-
-
-os.makedirs(os.path.dirname(os.path.abspath(log_file)), exist_ok=True)
-
 if directions:
     for direction in directions:
         for root, _, files in os.walk(direction):
@@ -45,4 +36,4 @@ if directions:
                 file_path = os.path.join(root, file)
                 for target in targets:
                     if target in read_file(file_path):
-                        write_file(log_file, '{} {}\n'.format(target, file_path))
+                        sys.stdout.write(f'{target} {file_path}\n')
